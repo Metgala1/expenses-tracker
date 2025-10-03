4076a8958cfa4514cd40a04e22eb232141ffb700
ATAGWE ROGER <atagweroger2@gmail.com>
2025-10-03 20:05:16 +0100

    Built my print expenses function

--- index.py
+++ index.py
@@ -1,2 +1,7 @@
 def add_expenses(expenses, amount, category) :
     expenses.append({'amount': amount, 'category': category})
+
+def print_expenses(expenses) :
+    for expense in expenses :
+        print(f'Amount: {expense["amount"]}, Category: {expense["category"]}')
+
